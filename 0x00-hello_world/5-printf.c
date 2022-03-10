1695a32b9a61739e035062f32e0a389f0f231f2a
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-03-10 04:52:51 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main - print with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
