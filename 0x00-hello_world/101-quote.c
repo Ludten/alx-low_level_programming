af6c4eeb5b482f618689073d1287bda40cca1f13
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-03-10 05:05:17 -0800

    return

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+/**
+ * main - print and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: 1 to standard error
+ */
+#include <stdio.h>
+
+int main()
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
