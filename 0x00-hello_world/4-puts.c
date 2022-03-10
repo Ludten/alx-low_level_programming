cc741919de8ddf61c062f33eeb9de82a8a90f7fc
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-03-10 04:41:59 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-/*Print "Programming is like building a multilingual puzzle"*/
+/**
+ * main - Print "Programming is like building a multilingual puzzle
+ */
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
