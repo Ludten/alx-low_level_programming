11498aaa230917feea74391e95219726d7f3a132
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-03-10 06:00:45 -0800

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,7 @@
 /**
- * main - print out the size of various types on the computer it is compiled and
+ * main - print out the size of various types
+ * on the computer it is compiled and
  * run on.
- *  @intType: int operand
- * @floatType: float operand
- * @longintType: long int operand
- * @longlintType: long long int operand
- * @charType: char operand
  * Return: 0
  */
 
@@ -13,17 +9,11 @@
 
 int main(void)
 {
-	int intType;
-	float floatType;
-	long int longintType;
-	long long int longlintType;
-	char charType;
-
-	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlintType));
-	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
