d2b4228d9525dbcd2584564e6f04828478e0a127
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-03-15 16:30:23 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,36 +5,39 @@
  */
 void print_times_table(int n)
 {
-	int i, j, s;
+	int row, column, s;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
+		for (row = 0; row <= n; row++)
 		{
-			_putchar('0' + s);
-			for (j = 0; j <= n; j++)
+			for (column = 0; column <= n; column++)
 			{
-				s = i * j;
-				_putchar(',');
-				if (s <= 9)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
+				s = (row * column);
+				if (column == 0)
 					_putchar('0' + s);
-				}
-				else if (s > 9 && s < 100)
+				else
 				{
+					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + (s / 10));
-					_putchar('0' + (s % 10));
-				}
-				else if (s > 99)
-				{
-					_putchar('0' + (s / 100));
-					_putchar('0' + ((s / 10) % 10));
-					_putchar('0' + (s % 10));
+					if (s <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + s);
+					}
+					else if (s > 9 && s < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (s / 10));
+						_putchar('0' + (s % 10));
+					}
+					else if (s >= 100)
+					{
+						_putchar('0' + (s / 100));
+						_putchar('0' + ((s / 10) % 10));
+						_putchar('0' + (s % 10));
+					}
 				}
 			}
 			_putchar('\n');
