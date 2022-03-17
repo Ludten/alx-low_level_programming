b484539e7cbd2118a6273ba9e08c6f9f7b4ba190
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-03-17 14:14:27 -0700

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,13 +10,16 @@ void print_line(int n)
 {
 	int c = 0;
 
-	while (c <= n)
+	if (n > 0)
 	{
-		if (n > 0)
+		while (c <= n)
 		{
 			_putchar('_');
+			c++;
 		}
-		c++;
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
+
 }
