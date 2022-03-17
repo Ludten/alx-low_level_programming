7a50ffdfc80fd0d576e55292428748624f78688b
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-03-17 16:42:06 -0700

    prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - highest prime
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int div = 2;
+	long int HFact;
+
+	while(n != 0)
+	{
+		if (n % div != 0)
+		{
+			div = div + 1;
+		}
+		else
+		{
+			HFact = n;
+			n = n / div;
+			if (n == 1)
+			{
+				printf("%ld\n", HFact);
+				break;
+			}
+		}
+	}
+	return (0);
+}
