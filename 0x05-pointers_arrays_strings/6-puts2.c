7fc7b54e564a974033bda4f03670ec5429ca6ba6
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-03-21 07:41:07 -0700

    every other

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other
+ *
+ * @str: string operand
+ *
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	_putchar(str[0]);
+
+	for (i = 0, j = 2; str[i + j] != 0; i++, j++)
+	{
+
+		_putchar(str[i + j]);
+	}
+	_putchar('\n');
+}
