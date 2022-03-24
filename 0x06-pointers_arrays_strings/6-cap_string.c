a4929062e0246386e5dcc0292f81052de92ca1aa
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-03-24 06:35:51 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @n: string operand
+ *
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	if (s[i] >= 'a' && s[i] <= 'z')
+	{
+		s[i] = s[i] - 32;
+	}
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		switch (s[i])
+		{
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+		case ' ':
+		case '\n':
+		case '\t':
+			if (s[i + 1] > 96 && s[i + 1] < 123)
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
