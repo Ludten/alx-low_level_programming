dd7a9057bb82a2c42dfca8b9f0c94eae69efa372
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-03-28 15:53:22 -0700

    str

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * *_strchr - locates a character in a string
@@ -9,6 +10,7 @@
  *
  * Return: s - 1 or NULL Success
  */
+
 char *_strchr(char *s, char c)
 {
 	int x;
