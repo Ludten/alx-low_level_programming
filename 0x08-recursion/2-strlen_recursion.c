9085865c8de78d215e6d7b763a17e4c5304b47d0
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-03-30 05:39:47 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - string length
+ *
+ * @s: input string
+ *
+ * Return: i Success
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}
