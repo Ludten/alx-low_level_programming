c2da90a93a8ff2f58f05d9b8e6069d1ec4008b45
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-03-30 06:30:07 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+int _palindrome(char *s, int i, int l);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - check for palindrome
+ *
+ * @s: string operand
+ *
+ * Return: 1 or 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+	{
+		return (1);
+	}
+	return (_palindrome(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - length of a string
+ *
+ * @s: string to operand
+ *
+ * Return: length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * _palindrome - check for palindrome
+ * @s: string operand
+ *
+ * @i: iterator
+ *
+ * @l: length
+ *
+ * Return: 1 or 0
+ */
+
+int check_pal(char *s, int i, int l)
+{
+	if (*(s + i) != *(s + l - 1))
+	{
+		return (0);
+	}
+	if (i >= l)
+	{
+		return (1);
+	}
+	return (_palindrome(s, i + 1, l - 1));
+}
