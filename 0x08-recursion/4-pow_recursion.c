5d963ecb932bef3c726627468554e7d9d3c620fe
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-03-30 05:52:18 -0700

    exp

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - exponential of a number
+ *
+ * @x: input variable
+ *
+ * @x: expon variable
+ *
+ * Return: pow Success
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
