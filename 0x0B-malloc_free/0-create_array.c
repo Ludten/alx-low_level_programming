2343b37fb0cd05e5acee441802c5b7eaef772399
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-04-05 07:09:22 -0700

    malloc array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * *create_array - create an array with malloc
+ *
+ * @size: size of array
+ *
+ * @c: input string
+ *
+ * Return: ptr or NULL (Error)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	int i;
+
+	if (size == 0)
+	{
+		return ("NULL");
+	}
+	else
+	{
+		ptr = malloc(size * sizeof(c));
+
+		if (ptr == NULL)
+		{
+			return ("NULL");
+		}
+		else
+		{
+			for (i = 0; i < size; i++)
+			{
+				ptr[i] = c;
+			}
+			return (ptr);
+		}
+	}
+}
