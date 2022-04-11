eae3320544c53d1af7b3ddc00d629a6c0c021bc7
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-04-11 18:00:50 +0100

    structs

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * print_dog - print dog details
+ *@d - dog
+ *
+ */
+void print_dog(struct dog *d)
+{
+    if ((*d).name == NULL)
+        (*d).name = "(nil)";
+    if ((*d).owner == NULL)
+        (*d).owner = "(nil)";
+
+    printf("Name: %s\nAge: %0.6f\nOwner: %s\n",
+           (*d).name, (*d).age, (*d).owner);
+}
