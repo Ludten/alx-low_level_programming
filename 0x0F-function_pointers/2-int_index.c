ec2907ee7d6269ceb50d78966ae3ac597c88d7ac
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-04-13 19:02:09 +0100

    function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,19 +15,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 	return (-1);
 
-	int i;
-
 	if (array != NULL && cmp != NULL)
 	{
+		int i;
+
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(*(array +i)))
 			return (i);
 		}
 	}
-
-	if (cmp(*(array + i) == 0))
-	return (-1);
-
-	return(0);
+	return(-1);
 }
