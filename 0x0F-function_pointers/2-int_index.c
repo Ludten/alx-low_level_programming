2184521e8853419624be1d4fab74780f8ad0ed1a
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-04-13 18:42:44 +0100

    function pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - find array element
+ * @array: array of numbers
+ * @size: size of the array
+ * @cmp: pointer to the function
+ *
+ * Return: 0 or 1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+	return (-1);
+
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		return (i);
+	}
+
+	if (cmp(array[i] == 0))
+	return (-1);
+}
