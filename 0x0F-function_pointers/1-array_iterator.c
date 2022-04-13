1ca5df7bf79cf03f34ffa8c65752f08867f633bf
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-04-13 18:33:40 +0100

    function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - print array elements
+ * @array: array of numbers
+ * @size: size of the array
+ * @action: pointer to the function
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
