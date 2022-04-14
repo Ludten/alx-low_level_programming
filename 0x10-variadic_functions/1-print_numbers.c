d0b4b531ec551edea90e8b454b45dc5a865bad57
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-04-14 23:35:38 +0100

    variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,9 +25,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(ap, int));
 		if (i < n - 1)
 		{
-			if (separator == NULL)
-			;
-			else
+			if (separator != NULL)
 			printf("%s", *separator);
 		}
 	}
