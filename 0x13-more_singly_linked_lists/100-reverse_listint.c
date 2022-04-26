d949fb9e1dcb82109496da17be5903e66b84edda
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-04-26 22:51:57 +0100

    reverse task

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse list
+ *
+ * @head: head
+ * Return: pointer to head node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *first;
+	listint_t *rest;
+
+	if (*head == NULL)
+		return (NULL);
+
+	first = *head;
+	rest = first->next;
+	if (rest == NULL)
+		return (NULL);
+	reverse_listint(&rest);
+
+	first->next->next = first;
+	first->next = NULL;
+	*head = rest;
+	return (*head);
+}
