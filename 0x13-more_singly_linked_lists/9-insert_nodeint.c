64916235eae842d1f0c20f698cdc8951a55838e1
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-04-26 19:02:42 +0100

    correction for insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,12 +15,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp2 = *head;
 	unsigned int i;
 
+	if (*head == NULL)
+		return (NULL);
 	if (temp1 == NULL)
 		return (NULL);
 	temp1->n = n;
 	temp1->next = NULL;
-	if (*head == NULL)
-		return (NULL);
+
+	if (index == 0)
+	{
+		temp1->next = temp2;
+		*head = temp1;
+		return (temp1);
+	}
 
 	i = 0;
 	while (i < (idx - 1))
