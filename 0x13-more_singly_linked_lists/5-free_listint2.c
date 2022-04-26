b74075c5ee13bb46efb08186888082316b65ad27
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-04-26 16:56:45 +0100

    correction for free task

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,9 +7,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (*head == NULL)
+	listint_t *temp;
+
+	if (head == NULL)
 		return;
-	if ((*head)->next)
-		free_listint2(&(*head)->next);
-	free(*head);
+
+	while (*head)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
 }
