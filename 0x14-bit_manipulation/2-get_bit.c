eca95132f60aef1c96bc865db36705b79b1e2e9d
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-04-28 19:06:23 +0100

    changed condition

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * get_bit - Get the bit object
+ *
+ * @n: decimal
+ * @index: index
+ * Return: bit
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int a, b, c;
 
-	if (index >= 0 && index <= 31)
+	if (index <= 31)
 	{
 		a = 1 << index;
 		b = n & a;
