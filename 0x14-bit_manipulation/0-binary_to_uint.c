dbc7f40aaec707f2d8cd800633d1eba13d18bb64
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-04-28 16:11:29 +0100

    correction for exp

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,6 +14,26 @@ int _isdigit(unsigned int c)
 }
 
 /**
+ * _pow - solve exp
+ *
+ * @a: base
+ * @b: exp
+ * Return: value
+ */
+unsigned int _pow(unsigned int a, unsigned int b)
+{
+	unsigned int i, c;
+
+	if (b == 0)
+		return (1);
+	c = 1;
+
+	for (i = 0; i < b; i++)
+		c *= a;
+	return (c);
+}
+
+/**
  * binary_to_uint - convert string binary to
  * decimal
  *
@@ -34,7 +54,7 @@ unsigned int binary_to_uint(const char *b)
 		a = (b[len - i - 1]) - 48;
 		if (_isdigit(a))
 			return (0);
-		dec += a * ((int)pow(2, i));
+		dec += a * (_pow(2, i));
 	}
 	return (dec);
 }
