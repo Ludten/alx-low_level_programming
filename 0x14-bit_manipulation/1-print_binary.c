323df470bd1cb82b8004edb5c5b8ab4702107c58
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-04-28 17:48:01 +0100

    bit man

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,3 +1,24 @@
 #include "main.h"
 
+/**
+ * print_binary - print converted decimal to
+ * binary
+ *
+ * @n: decimal to be converted
+ */
 void print_binary(unsigned long int n)
+{
+	unsigned int mask = n;
+
+	if (n == 0)
+		_putchar('0');
+
+	while (mask > 0)
+	{
+		if ((n & mask) == 0)
+			_putchar('0');
+		else
+			_putchar('1');
+		mask = mask >> 1; /* Right Shift */
+	}
+}
