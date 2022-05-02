08c6ba8007710f47c65851613d76e21bcbdbe055
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-05-03 00:25:59 +0100

    malloc correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,13 +12,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t n, m;
-	char *buff = malloc(sizeof(char) * (letters));
+	char *buff;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY); /*read*/
 
 	if (fd == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
 		return (0);
 	m = read(fd, buff, letters);
 	close(fd);
