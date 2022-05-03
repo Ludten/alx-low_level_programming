b3e7950019f498de50734819b918d4c5829b5365
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-05-03 01:12:15 +0100

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 	i = 0;
-	while (*text_content)
+	while (text_content[i])
 		i++;
 	n = write(fd, text_content, i);
 	if (n == -1)
