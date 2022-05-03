c872a9519b9a7834befd93796ae0b9769c5c0c19
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-05-03 02:36:59 +0100

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,77 +4,6 @@
 #include <stddef.h>
 #include <fcntl.h>
 
-void read_textfile(const char *filename0, const char *filename1);
-void create_file(const char *filename, char *buff, size_t bt);
-/**
- * read_textfile - read a text file and print
- * it to the POSIX standard output.
- *
- * @filename0: name of text file
- * @filename1: name of text file
- * Return: number of bytes printed or 0
- */
-void read_textfile(const char *filename0, const char *filename1)
-{
-	int fd;
-	ssize_t m;
-	char buffer[1024];
-
-	if (filename0 == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename0);
-		exit(98);
-	}
-	fd = open(filename0, O_RDONLY); /*read*/
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename0);
-		exit(98);
-	}
-	m = read(fd, buffer, 1024);
-	if (m == -1)
-	{
-		close(fd);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename0);
-		exit(98);
-	}
-	close(fd);
-	create_file(filename1, buffer, m);
-}
-
-/**
- * create_file - Create a file
- *
- * @filename: file name
- * @buff: buffer
- * @bt: no. of bytes
- */
-void create_file(const char *filename, char *buff, size_t bt)
-{
-	int fd, n;
-
-	if (filename == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", filename);
-		exit(99);
-	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00664);
-
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", filename);
-		exit(99);
-	}
-	n = write(fd, buff, bt);/*write*/
-	if (n == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", filename);
-		close(fd);
-		exit(99);
-	}
-	close(fd);
-}
-
 /**
  * main - copy files
  *
@@ -84,11 +13,45 @@ void create_file(const char *filename, char *buff, size_t bt)
  */
 int main(int ac, char **av)
 {
+	int fd[2];
+	int r, a, b;
+	char buf[BUFSIZ];
+
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	read_textfile(av[1], av[2]);
+	fd[0] = open(av[1], O_RDONLY);
+	if (fd[0] < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	fd[1] = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((r = read(fd[0], buf, BUFSIZ)) > 0)
+	{
+		if (fd[1] < 0 || write(fd[1], buf, r) != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			close(fd[0]);
+			exit(99);
+		}
+	}
+	if (r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	a = close(fd[0]);
+	b = close(fd[1]);
+	if (a < 0 || b < 0)
+	{
+		if (a < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd[0]);
+		if (b < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd[1]);
+		exit(100);
+	}
 	return (0);
 }
