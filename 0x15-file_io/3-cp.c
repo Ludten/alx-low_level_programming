d3850b92cd7c32935d7f2f40f6242e8e40e32b81
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-05-03 02:31:45 +0100

    corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,20 +22,20 @@ void read_textfile(const char *filename0, const char *filename1)
 
 	if (filename0 == NULL)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", filename0);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename0);
 		exit(98);
 	}
 	fd = open(filename0, O_RDONLY); /*read*/
 	if (fd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", filename0);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename0);
 		exit(98);
 	}
 	m = read(fd, buffer, 1024);
 	if (m == -1)
 	{
 		close(fd);
-		dprintf(2, "Error: Can't read from file %s\n", filename0);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename0);
 		exit(98);
 	}
 	close(fd);
@@ -55,20 +55,20 @@ void create_file(const char *filename, char *buff, size_t bt)
 
 	if (filename == NULL)
 	{
-		dprintf(2, "Error: Can't write to file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", filename);
 		exit(99);
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00664);
 
 	if (fd == -1)
 	{
-		dprintf(2, "Error: Can't write to file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", filename);
 		exit(99);
 	}
 	n = write(fd, buff, bt);/*write*/
 	if (n == -1)
 	{
-		dprintf(2, "Error: Can't write to file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", filename);
 		close(fd);
 		exit(99);
 	}
@@ -86,9 +86,9 @@ int main(int ac, char **av)
 {
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	read_textfile(av[2], av[3]);
+	read_textfile(av[1], av[2]);
 	return (0);
 }
