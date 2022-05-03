9450d7f971dfdca51661ab996959100dca164d4a
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-05-03 01:10:00 +0100

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
 
 	if (fd == -1)
 		return (-1);
@@ -24,9 +24,9 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 	i = 0;
-	while (text_content)
+	while (*text_content)
 		i++;
-	n = read(fd, text_content, i);
+	n = write(fd, text_content, i);
 	if (n == -1)
 	{
 		close(fd);
