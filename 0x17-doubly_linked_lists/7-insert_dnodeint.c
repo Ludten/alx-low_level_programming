3a95cff7a1a3e870ceee896a4b45de9b4b33193a
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-09 23:51:49 +0100

    The best

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,6 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
 	dlistint_t *temp1, *temp2;
 
 	if (*h == NULL)
@@ -20,23 +19,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	for (i = 0; temp2 && i < idx; i++)
+	for (; idx != 1; idx--)
 	{
-		if (i == idx - 1)
-		{
-			if (temp2->next == NULL)
-				return (add_dnodeint_end(h, n));
-			temp1 = malloc(sizeof(dlistint_t));
-			if (temp1 == NULL)
-				return (NULL);
-			temp1->next = temp2->next;
-			temp1->prev = temp2;
-			temp2->next->prev = temp1;
-			temp2->next = temp1;
-			return (temp1);
-		}
-		else
-			temp2 = temp2->next;
+		temp2 = temp2->next;
+		if (temp2 == NULL)
+			return (NULL);
 	}
-	return (NULL);
+
+	if (temp2->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	temp1 = malloc(sizeof(dlistint_t));
+	if (temp1 == NULL)
+		return (NULL);
+
+	temp1->n = n;
+	temp1->prev = temp2;
+	temp1->next = temp2->next;
+	temp2->next->prev = temp1;
+	temp2->next = temp1;
+
+	return (temp1);
 }
