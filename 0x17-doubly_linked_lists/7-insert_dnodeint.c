12e865d6662014101c877b755f1eca58e6956bfb
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-09 23:36:43 +0100

    Changed algo

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,43 +7,36 @@
  * @h: head node
  * @idx: index
  * @n: node data
- * Return: Address of new node
+ * Return: Address of temp1 node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
-	dlistint_t *last, *temp1, *temp2;
+	dlistint_t *temp1, *temp2;
 
 	if (*h == NULL && idx > 0)
 		return (NULL);
+	temp1 = malloc(sizeof(dlistint_t));
+	if (temp1 == NULL)
+		return;
+	temp2 = *h;
 
-	last = *h;
-	temp2 = last;
-	i = 1;
-	while (i < idx - 1 && temp2 != NULL)
-	{
-		temp2 = temp2->next;
-		i++;
-	}
-	if (idx == 1)
-	{
+	if (idx == 0)
 		return (add_dnodeint(h, n));
-	}
-	else if (temp2 == *h)
+	for (i = 0; temp2 && i < idx; i++)
 	{
-		return (add_dnodeint_end(h, n));
-	}
-	else if (temp2 != NULL)
-	{
-		temp1 = malloc(sizeof(dlistint_t));
-		if (temp1 == NULL)
-			return (NULL);
-		temp1->n = n;
-		temp1->next = temp2->next;
-		temp1->prev = temp2;
-		if (temp2->next != NULL)
+		if (i == idx - 1)
+		{
+			if (temp2->next == NULL)
+				return (add_dnodeint_end(h, n));
+			temp1->next = temp2->next;
+			temp1->prev = temp2;
 			temp2->next->prev = temp1;
-		temp2->next = temp1;
+			temp2->next = temp1;
+			return (temp1);
+		}
+		else
+			temp2 = temp2->next;
 	}
-	return (temp1);
+	return (NULL);
 }
