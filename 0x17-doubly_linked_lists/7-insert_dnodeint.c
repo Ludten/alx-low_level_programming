f6f49f4d7a05a3813b091d11ec094288916611b5
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-10 00:12:24 +0100

    Deep dark

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,37 +11,24 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
-	dlistint_t *temp1, *temp2, *next;
+	dlistint_t *temp1 = NULL, *temp2 = *h;
 
-	if (*h == NULL)
-		return (NULL);
-	if (idx != 0)
+	while (temp2 && idx > 0)
+		temp2 = temp2->next, idx--;
+
+	if (idx == 0 && temp2 != NULL)
 	{
-		temp2 = *h;
-		for (i = 0; i < idx - 1 && temp2 != NULL; i++)
-			temp2 = temp2->next;
-		if (temp2 == NULL)
+		temp1 = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+		if (temp1 == NULL)
 			return (NULL);
+
+		temp1->n = n, temp1->prev = NULL, temp1->next = NULL;
+
+		(temp2->prev)->next = temp1;
+		temp1->prev = temp2->prev;
+		temp1->next = temp2;
+		temp2->prev = temp1;
 	}
-	temp1 = malloc(sizeof(dlistint_t));
-	if (temp1 == NULL)
-		return (NULL);
-	temp1->n = n;
-	if (idx == 0)
-	{
-		next = *h;
-		*h = temp1;
-		temp1->prev = NULL;
-	}
-	else
-	{
-		temp1->prev = temp2;
-		next = temp2->next;
-		temp2->next = temp1;
-	}
-	temp1->next = next;
-	if (temp1->next != NULL)
-		temp1->next->prev = temp1;
 	return (temp1);
 }
