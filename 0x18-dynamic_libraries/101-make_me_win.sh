b1f7c07e134c431b7924bfb4ead7acd79d061de8
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-13 22:48:05 +0100

    Sometimes I do things that surprises even me

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/giga.so https://github.com/Nkem-wey-sabi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/giga.so
+wget -O /tmp/giga.so https://github.com/Ludten/alx-low_level_programming/raw/master/0x18-dynamic_libraries/giga.so
 export LD_PRELOAD=/tmp/giga.so
