87c4cad618b65e482432e0eec49edea0ac4268b0
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-13 22:23:45 +0100

    Corrections

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget --continue --output-document=/tmp/giga.so https://github.com/SlamChillz/alx-low_level_programming/raw/master/0x18-dynamic_libraries/giga.so
-export LD_PRELOAD=/tmp/jackpot.so:$LD_PRELOAD
+wget -O /tmp/giga.so https://github.com/Nkem-wey-sabi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/giga.so
+export LD_PRELOAD=/tmp/giga.so:$LD_PRELOAD
