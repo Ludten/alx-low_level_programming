a9b7634c88efeea30b0635c21df198f8051fe226
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-13 22:52:24 +0100

    changed name

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget --continue --output-document=/tmp/giga.so https://github.com/Ludten/alx-low_level_programming/raw/master/0x18-dynamic_libraries/giga.so
-export LD_PRELOAD=/tmp/giga.so
+wget --continue --output-document=/tmp/gigam.so https://github.com/Ludten/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gigam.so
+export LD_PRELOAD=/tmp/gigam.so
