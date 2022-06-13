eb607e131d0ff483e0af42e3c5649213449986ac
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-13 22:54:20 +0100

    take 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget --continue --output-document=/tmp/gigam.so https://github.com/Ludten/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gigam.so
-export LD_PRELOAD=/tmp/gigam.so
+wget --continue --output-document=/tmp/gigamil.so https://github.com/Ludten/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gigamil.so
+export LD_PRELOAD=/tmp/gigamil.so
