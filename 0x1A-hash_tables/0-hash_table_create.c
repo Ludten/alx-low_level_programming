7f3d3e37751aedba783230a4f47456d642fb7504
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-06-30 23:04:58 +0100

    cleared betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,23 +4,26 @@
  * hash_table_create - create a memory location
  * for the hash table
  *
- * size: size of array
+ * @size: size of array
  * Return: Address of the table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t));
+	table->array = malloc(sizeof(hash_node_t) * table->size);
 	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
