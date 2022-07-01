3a257958af36480180b85a880a69c03a2455a673
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-07-01 11:15:35 +0100

    hash tabling is dif

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,29 +14,31 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *elem, *curr_item;
 
-	if (!ht || !strcmp(key, "") || !key)
-		return (0);
-
-	idx = key_index((unsigned char *)key, ht->size);
-	curr_item = ht->array[idx];
-	while (curr_item != NULL)
+	if ((ht != NULL) && (ht->array != NULL)
+		&& (key != NULL) && (strlen(key) > 0))
 	{
-		if (strcmp(curr_item->key, key) == 0)
+		idx = key_index((unsigned char *)key, ht->size);
+		curr_item = ht->array[idx];
+		while (curr_item != NULL)
 		{
-			free(curr_item->value);
-			curr_item->value = strdup(value);
+			if (strcmp(curr_item->key, key) == 0)
+			{
+				free(curr_item->value);
+				curr_item->value = str_copy(value);
+				return (1);
+			}
+			curr_item = curr_item->next;
+		}
+		curr_item = ht->array[idx];
+		elem = malloc(sizeof(hash_node_t));
+		if (elem != NULL)
+		{
+			elem->key = str_copy(key);
+			elem->value = str_copy(value);
+			elem->next = curr_item;
+			ht->array[idx] = elem;
 			return (1);
 		}
-		curr_item = curr_item->next;
 	}
-	curr_item = ht->array[idx];
-	elem = malloc(sizeof(hash_node_t));
-	if (elem == NULL)
-		return (0);
-	elem->key = strdup(key);
-	elem->value = strdup(value);
-	elem->next = curr_item;
-	ht->array[idx] = curr_item;
-
-	return (1);
+	return (0);
 }
