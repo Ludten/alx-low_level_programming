f0e38d07617faecacf96f7369e3dd7dbbb3de7df
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-10-05 19:39:56 +0100

    Search Algorithms
    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -29,15 +29,14 @@ int jump_search(int *array, size_t size, int value)
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", prev, step);
 
-	while (array[prev] < value && prev < size)
+	while (array[prev] <= value && prev < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
-		prev++;
 		if (array[prev] == value)
 		{
-			printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
 			return (prev);
 		}
+		prev++;
 	}
 
 	return (-1);
