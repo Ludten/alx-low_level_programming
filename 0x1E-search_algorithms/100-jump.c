44cd7f6c4763cc735f865745854c9f719f010d3a
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-10-05 09:27:09 +0100

    Search Algorithms
    jump search algo that actually jumps

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - Searches for a value in a sorted array of integers using the
+ * Jump search algorithm
+ *
+ * @array: array to search
+ * @size: size of array
+ * @value: value to be searched for
+ * Return: -1 OR index
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t step, prev;
+
+	if (!array)
+		return (-1);
+
+	prev = 0;
+	step = sqrt(size);
+	printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+
+	while (array[step] < value && prev < size - 1)
+	{
+		prev = step;
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		step += sqrt(size);
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", prev, step);
+
+	while (array[prev] < value && prev < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		prev++;
+		if (array[prev] == value)
+		{
+			printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+			return (prev);
+		}
+	}
+
+	return (-1);
+}
