9a47c658ee66ac7b3444f10dcf702492f244b5a1
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-10-05 22:23:59 +0100

    Search Algorithms
    Mendie's correction

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -11,41 +11,34 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t i, prev, step;
-	listint_t *temp;
+	size_t i, step;
+	listint_t *temp = list;
 
 	if (list == NULL)
 		return (NULL);
-	step = sqrt(size);
-	prev = 0;
-	temp = list;
-	while (temp->n < value && step < size && temp)
+	step = sqrt((double) size), i = list->index + step;
+	for (; list != NULL; list = list->next)
 	{
-		for (i = prev; temp && (i < step); ++i)
-			temp = temp->next;
-		printf("Value checked array[%ld] = [%d]\n", temp->index, temp->n);
-		prev = step;
-		step += sqrt(size);
+		if (list->index == i)
+		{
+			printf("Value checked at index [%ld] = [%d]\n",
+					list->index, list->n);
+			if (list->n < value && list->next)
+				temp = list, i = i + step;
+			else
+				break;
+			i = i < size ? i : size - 1;
+		}
 	}
-	step = step < size ? step : size - 1;
-	if (value < temp->n)
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			temp->index, i);
+	for (; temp && temp->index <= i &&
+			temp->n <= value; temp = temp->next)
 	{
-		for (i = 0; list && (i < prev - sqrt(size)); i++)
-			list = list->next;
-		printf("Value found between indexes [%ld] and [%ld]\n", list->index, prev);
-	}
-	else
-	{
-		for (i = 0; i < prev && (list); i++)
-			list = list->next;
-		printf("Value found between indexes [%ld] and [%ld]\n", list->index, step);
-	}
-	while (list && list->n <= value && prev < step)
-	{
-		printf("Value checked array[%ld] = [%d]\n", list->index, list->n);
-		if (list->n == value)
-			return (list);
-		list = list->next;
+		printf("Value checked at index [%ld] = [%d]\n",
+				temp->index, temp->n);
+		if (temp->n == value)
+			return (temp);
 	}
 	return (NULL);
 }
